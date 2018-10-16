4672db29255a5d6c527a669cb618542633451aac
anastafj197 <frankie.anastasia@gmail.com>
2018-10-16 15:52:32 -0400

    Review of lesson one
    single / multi line comments 
    print / puts statements 
    simple methods on strings

---
+++ lesson_1.rb
@@ -0,0 +1,17 @@
+# Single line comment 
+
+=begin 
+Multi
+Line 
+Comment 
+=end
+
+# puts is print with newline 
+puts "Greg"
+print "Poe"
+
+# pretty self explanintory 
+puts "Greg".length
+puts "Greg".reverse 
+puts "GREG".downcase 
+puts "greg".upcase 
